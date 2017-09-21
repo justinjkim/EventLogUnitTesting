9a79544f0933ce174b06174994fe5edb04fb9100
Justin Kim <jjkim928@gmail.com>
2017-09-21 14:52:18 -0400

    added getter and setter methods

--- src/com/company/Event.java
+++ src/com/company/Event.java
@@ -3,4 +3,20 @@ package com.company;
 public class Event {
     String name;
     String action;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
 }
