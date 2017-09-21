d47ec4c97235ca13e767f6436083137946630537
Justin Kim <jjkim928@gmail.com>
2017-09-21 15:14:45 -0400

    ok, think I got a null test to work

--- test/com/company/EventLogTest.java
+++ test/com/company/EventLogTest.java
@@ -1,4 +1,18 @@
 package com.company;
 
+import org.junit.Test;
+
 public class EventLogTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void NullEventExpectsFalse() {
+        //arrange
+        EventLog eventlog = new EventLog();
+
+        //act
+        eventlog.addEvent(null);
+
+        //assert
+    }
+
 }
