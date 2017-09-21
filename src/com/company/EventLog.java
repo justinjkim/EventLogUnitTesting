386c959b3812766acaa1ffcbd4cd07adcb5472f7
Justin Kim <jjkim928@gmail.com>
2017-09-21 14:51:10 -0400

    added condition to check if event is null

--- src/com/company/EventLog.java
+++ src/com/company/EventLog.java
@@ -1,12 +1,17 @@
 package com.company;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EventLog {
 
-    private List<Event> eventList;
-    public boolean addEvent(Event event);
+    private List<Event> eventList = new ArrayList<>();
+    public boolean addEvent(Event event) {
+        if (event == null) {
+            throw new IllegalArgumentException();
+        }
+    };
     public int getNumEvents();
     public EventLog();
 }
