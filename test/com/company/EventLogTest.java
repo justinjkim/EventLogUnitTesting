af70f51a229326805ff2c93f9e9a2cb37f8e67a3
Justin Kim <jjkim928@gmail.com>
2017-09-21 15:19:48 -0400

    whoops, forgot to check

--- test/com/company/EventLogTest.java
+++ test/com/company/EventLogTest.java
@@ -2,10 +2,13 @@ package com.company;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
 public class EventLogTest {
 
     @Test(expected = IllegalArgumentException.class)
-    public void NullEventExpectsFalse() {
+    public void eventIsNullExpectFalse() {
         //arrange
         EventLog eventlog = new EventLog();
 
@@ -13,6 +16,22 @@ public class EventLogTest {
         eventlog.addEvent(null);
 
         //assert
+    }
+
+    @Test //(expected = IllegalArgumentException.class)
+    public void eventNameIsNullExpectFalse() {
+        //arrange
+        EventLog eventlog = new EventLog();
+        Event event = new Event();
+        event.setAction("Dance away!");
+
+        //act
+        eventlog.addEvent(event);
+
+        //assert
+        assertFalse("You cannot have an event without a name", event.name == null);
+
+
     }
 
 }
